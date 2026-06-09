33a0b32371063b77ae6a4439f1f51c9bf22cd499
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2026-06-09 17:18:40 +0800

    tokyo night theme in nvim and ghostty

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "tokyonight-night",
     },
   },
 }
